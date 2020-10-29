f67271ef06c189f8d3b727d689632872b4b12e83
Troy Sincomb <troysincomb@gmail.com>
2020-10-29 10:45:07 -0700

    naming clean up

--- gs_tools/pandas_from_gs.py
+++ gs_tools/pandas_from_gs.py
@@ -14,8 +14,8 @@ def df_from_gs(name: str,
     Pandas DataFram from Google Sheet
 
     Args:
-        name (str): Name of the Google Sheet Doc
-        sheet_name (str): Name of the Google Sheet 
+        name (str): Name or Key of the Google Sheet Doc.
+        sheet_name (str): Name or Key of the Google Sheet.
         header (list, optional): Column headers for selected rows/columns. Defaults to None which is the first row.
         row_start (int, optional): Index start of row. Defaults to 0.
         row_end (int, optional): Index end of row. Defaults to None.
@@ -25,11 +25,12 @@ def df_from_gs(name: str,
     Returns:
         pd.DataFrame: Only 1 df for the specified sheet.
     """
-    gc = gspread.oauth()  # add OAuth2 as ~/.config/gspread/credentials.json; will prop if missing something
-    sparc_proctur = gc.open(name)
-    worksheet = sparc_proctur.worksheet(sheet_name)
+    gs = gspread.oauth()  # add OAuth2 as ~/.config/gspread/credentials.json; will prop if missing something
+    gs_doc = gs.open(name)
+    sheet = gs_doc.worksheet(sheet_name)
+    rows = sheet.get_all_values()
     # cut ends of rows
-    rows = worksheet.get_all_values()[row_start:row_end]
+    rows = rows[row_start:row_end]
     # cut ends of columns
     rows = [row[column_start:column_end] for row in rows]
     # pull header from first row if not provided
